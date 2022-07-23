482df8d79623fa760eb6f70461daa3e7c7174af3
TracyJay <108913540+TracyJay@users.noreply.github.com>
2022-07-23 14:05:39 +1000

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x7B9b4F6AB1979A6923FAF4E8dD1F954AB555272A"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
